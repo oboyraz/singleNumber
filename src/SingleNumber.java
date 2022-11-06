57d6b94e6f1b445eae9cc15ad15e9bba127c964a
oboyraz <oboyraz@sakarya.edu.tr>
2022-11-06 03:52:38 +0300

    singleNumber

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -1,4 +1,10 @@
 public class SingleNumber {
+    /*
+    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+to see question => https://leetcode.com/problems/single-number/
+     */
     public static void main(String[] args) {
         int[] arr = {4,1,2,1,2};
         //int[] arr = {2,2,1};
